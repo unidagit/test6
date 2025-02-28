2ec60ae1392e699ea901f2aab32ee4a16a163313
uni <rhcjrdbsgml@gmail.com>
2025-02-28 20:34:53 +0900

    fix: 수정완료

--- test.tsx
+++ test.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function test() {
-  return <div>test</div>;
+  return (
+    <div>
+      <h1>djdjdjdj</h1>
+      <p>test이거는 테스트입니다</p>
+    </div>
+  );
 }
